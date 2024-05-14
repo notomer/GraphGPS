a1ca2907cdb94e0a76b6836afa34a10fdb97cc89
Omer Khan <notomerkhan@gmail.com>
2024-05-14 06:32:48 -0400

    Initial Commit

---
+++ GraphGPS/GraphGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphGPSApp.swift
+//  GraphGPS
+//
+//  Created by Omer Khan on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GraphGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
